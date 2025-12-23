62c4710ee5789fdf2ecff2794e1cc1e0511f598d
Aditya Rajagopal <adi.raj465@gmail.com>
2025-12-22 19:48:57 -0800

    feat: slight refactoring of nob.c to prepare for windows support

--- nob.c
+++ nob.c
@@ -32,49 +32,29 @@ static transfer_item items[] = {
 };
 #endif
 
+static const char* get_user_home(void);
+static void create_symlink(Cmd* cmd, transfer_item item, char* src_buf, char* dst_buf);
+
+static const char* home = NULL;
+static const char* cwd = NULL;
+
 int main(int argc, char** argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-#ifdef _WIN32
-    const char* home = getenv("USERPROFILE");
-    if (home == NULL) {
-        nob_log(NOB_ERROR, "Could not get USERPROFILE environment variable");
-        return 1;
-    }
-#else
-    const char* home = getenv("HOME");
-    if (home == NULL) {
-        nob_log(NOB_ERROR, "Could not get HOME environment variable");
-        return 1;
-    }
-#endif
-
     char src_buf[BUFFER_SIZE];
     char dst_buf[BUFFER_SIZE];
+    cwd = nob_get_current_dir_temp();
+    home = get_user_home();
+    if (home == NULL)
+        return 1;
+    if (cwd == NULL)
+        return 1;
 
-    // TODO: use async cmd_run() instead
+    // @TODO add ability to run other types of commands
     Procs procs = {0};
     Cmd cmd = {0};
     for (size_t i = 0; i < NOB_ARRAY_LEN(items); ++i) {
-        transfer_item item = items[i];
-#ifdef _WIN32
-        switch (item.type) {
-        case TYPE_DIRECTORY:
-            snprintf(src_buf, BUFFER_SIZE, "%s\\%s", home, item.src);
-            snprintf(dst_buf, BUFFER_SIZE, "%s\\%s", home, item.dst);
-            cmd_append(&cmd, "cmd", "/c", "mklink", "/J", dst_buf, src_buf);
-            break;
-        case TYPE_FILE:
-            snprintf(src_buf, BUFFER_SIZE, "%s\\%s", home, item.src);
-            snprintf(dst_buf, BUFFER_SIZE, "%s\\%s", home, item.dst);
-            cmd_append(&cmd, "cmd", "/c", "mlink", dst_buf, src_buf);
-            break;
-        }
-#else
-        snprintf(src_buf, BUFFER_SIZE, "%s/.dotfiles/%s", home, item.src);
-        snprintf(dst_buf, BUFFER_SIZE, "%s/%s", home, item.dst);
-        cmd_append(&cmd, "ln", "-sf", src_buf, dst_buf);
-#endif
+        create_symlink(&cmd, items[i], src_buf, dst_buf);
         if (!cmd_run(&cmd, .async = &procs, .max_procs = 8)) {
             nob_log(NOB_ERROR, "Could not create symlink %s -> %s", dst_buf, src_buf);
         }
@@ -83,4 +63,42 @@ int main(int argc, char** argv) {
     procs_flush(&procs);
 
     return 0;
+}
+
+static const char* get_user_home(void) {
+#ifdef _WIN32
+    const char* home = getenv("USERPROFILE");
+    if (home == NULL) {
+        nob_log(NOB_ERROR, "Could not get USERPROFILE environment variable");
+        return NULL;
+    }
+#else
+    const char* home = getenv("HOME");
+    if (home == NULL) {
+        nob_log(NOB_ERROR, "Could not get HOME environment variable");
+        return NULL;
+    }
+#endif
+    return home;
+}
+
+static void create_symlink(Cmd* cmd, transfer_item item, char* src_buf, char* dst_buf) {
+#ifdef _WIN32
+    switch (item.type) {
+    case TYPE_DIRECTORY:
+        snprintf(src_buf, BUFFER_SIZE, "%s\\%s", home, item.src);
+        snprintf(dst_buf, BUFFER_SIZE, "%s\\%s", home, item.dst);
+        cmd_append(cmd, "cmd", "/c", "mklink", "/J", dst_buf, src_buf);
+        break;
+    case TYPE_FILE:
+        snprintf(src_buf, BUFFER_SIZE, "%s\\%s", home, item.src);
+        snprintf(dst_buf, BUFFER_SIZE, "%s\\%s", home, item.dst);
+        cmd_append(cmd, "cmd", "/c", "mlink", dst_buf, src_buf);
+        break;
+    }
+#else
+    snprintf(src_buf, BUFFER_SIZE, "%s/%s", cwd, item.src);
+    snprintf(dst_buf, BUFFER_SIZE, "%s/%s", home, item.dst);
+    cmd_append(cmd, "ln", "-sf", src_buf, dst_buf);
+#endif
 }
